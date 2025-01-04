1b4c33eb759878bf1e1de8f15a575c9c244e244d
artemtimofeev <artemtimofeev@rambler.ru>
2025-01-04 16:57:00 +0300

    First develop commit. Users_app, migrations, swagger, cors.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "tg_id",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "avatar",
+    )
+    search_fields = ("email", "first_name", "last_name", "phone_number")
+    list_filter = ("email", "first_name", "last_name", "phone_number")
